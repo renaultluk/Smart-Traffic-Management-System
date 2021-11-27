3a6ed2582d41ccee765f02fda1f8455271091a0b
renaultluk <71740121+renaultluk@users.noreply.github.com>
2021-11-27 19:23:27 +0800

    new

--- server/c++.cpp
+++ server/c++.cpp
@@ -17,6 +17,7 @@ using namespace std;
 int number;
 int vehicle_id;
 int start_node_index;
+int target_node_index;
 
 void input_destination(){
     while (true){
@@ -24,15 +25,53 @@ void input_destination(){
         cout <<"1. initialize vehicle: init <VEHICLE_ID> <START_NODE_INDEX>"<<endl;
         cout <<"2. set vehicle destination: set <VEHICLE_ID> <TARGET_NODE_INDEX>"<<endl;
         cout <<"3. toggle vehicle mode: toggle <VEHICLE_ID>"<<endl;
+        cout <<"Please enter corresponding number: "<<endl;
         cin >> number;
 
         if (number == 1){
             cout <<"Please enter <VEHICLE_ID> and <START_NODE_INDEX>. "<<endl;
             cin >> vehicle_id;
             cin >> start_node_index;
-            
+            if ( vehicle_id == 1){
+                self.index = start_node_index;
+                
+                //has its corresponding x,y
 
+            }
+
+            if ( vehicle_id == 2){
+                self.index = start_node_index;
+                //has its corresponding x,y
+                
+            }
+
+            if ( vehicle_id == 3){
+                self.index = start_node_index;
+                //has its corresponding x,y
+                
+            }
+            
         }
+
+        if (number == 2){
+            cout <<"Please enter <VEHICLE_ID> and <TARGET_NODE_INDEX>. "<<endl;
+            cin >> vehicle_id;
+            cin >> target_node_index;
+            
+        }
+
+        if (number == 3){
+            cout <<"Please enter <VEHICLE_ID>. "<<endl;
+            cin >> vehicle_id;
+            
+        }
+
+        if (!(number == 1)||!(number == 2)||!(number == 3)){
+            cerr<<"Error: Invalid number of parameters. Please try again"<<endl;
+            break;
+        }
+
+
 
     }
 }
