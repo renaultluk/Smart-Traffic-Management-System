b9d09cd8ded634361d20bb99fa4709fc4f3957e2
renaultluk <ren66.rl@gmail.com>
2021-11-29 01:39:33 +0800

    Add test

---
+++ test/plan/graphStructs.h
@@ -0,0 +1,65 @@
+#ifndef GRAPHSTRUCTS_H
+#define GRAPHSTRUCTS_H
+
+#define NUM_NODES   17
+#define NUM_EDGES   26
+#define NUM_DEST    3
+
+struct Node {
+    int index;
+    int degree;
+    int edges[3];
+    int distance = __INT_MAX__;
+    Node* prev;
+    bool visited = false;
+};
+
+struct Edge {
+    int index;
+    Node* start;
+    Node* end;
+    double weight;
+};
+
+Node newBlankNode();
+
+Node newNode(int index);
+
+Edge newEdge(int index, Node* start, Node* end, int weight);
+
+Edge connectEdge(Node node1, Node node2);
+
+void initMap();
+
+Node node_map[NUM_NODES] = {};
+Node* destinations[NUM_DEST] = {};
+Edge edge_map[NUM_EDGES] = {
+    newEdge(0, &node_map[0], &node_map[1], 1),
+    newEdge(1, &node_map[1], &node_map[4], 2),
+    newEdge(2, &node_map[4], &node_map[10], 2),
+    newEdge(3, &node_map[10], &node_map[15], 4),
+    newEdge(4, &node_map[15], &node_map[19], 1),
+    newEdge(5, &node_map[15], &node_map[16], 3),
+    newEdge(6, &node_map[16], &node_map[17], 9),
+    newEdge(7, &node_map[17], &node_map[18], 1),
+    newEdge(8, &node_map[17], &node_map[14], 3),
+    newEdge(9, &node_map[14], &node_map[9], 3),
+    newEdge(10, &node_map[9], &node_map[3], 4),
+    newEdge(11, &node_map[3], &node_map[2], 8),
+    newEdge(12, &node_map[2], &node_map[1], 4),
+    newEdge(13, &node_map[2], &node_map[5], 2),
+    newEdge(14, &node_map[5], &node_map[4], 4),
+    newEdge(15, &node_map[5], &node_map[7], 1),
+    newEdge(16, &node_map[7], &node_map[11], 2),
+    newEdge(17, &node_map[11], &node_map[10], 3),
+    newEdge(18, &node_map[11], &node_map[12], 3),
+    newEdge(19, &node_map[12], &node_map[16], 3),
+    newEdge(20, &node_map[12], &node_map[13], 4),
+    newEdge(21, &node_map[13], &node_map[14], 4),
+    newEdge(22, &node_map[13], &node_map[8], 3),
+    newEdge(23, &node_map[8], &node_map[9], 4),
+    newEdge(24, &node_map[8], &node_map[6], 3),
+    newEdge(25, &node_map[6], &node_map[7], 3)
+};
+
+#endif
---
+++ test/plan/node_utils.ino
@@ -0,0 +1,50 @@
+#include "graphStructs.h"
+
+Node newBlankNode() {
+    Node node;
+    node.index = __INT_MAX__;
+    node.prev = nullptr;
+    node.visited = false;
+    node.distance = __INT_MAX__;
+    return node;
+}
+
+Node newNode(int index) {
+    Node node;
+    node.index = index;
+    node.prev = nullptr;
+    node.visited = false;
+    node.distance = __INT_MAX__;
+    return node;
+}
+
+Edge newEdge(int index, Node* start, Node* end, int weight) {
+    Edge edge;
+    edge.index = index;
+    edge.start = start;
+    edge.end = end;
+    edge.weight = weight;
+    return edge;
+}
+
+Edge* connectEdge(Node* node1, Node* node2) {
+    int map_length = sizeof(edge_map)/sizeof(*edge_map);
+    for (int i = 0; i < map_length; i++) {
+        if (edge_map[i].start->index == node1->index && edge_map[i].end->index == node2->index) {
+            return &edge_map[i];
+        }
+    }
+}
+
+void initMap() {
+    for (int i = 0; i < NUM_NODES; i++) {
+        node_map[i] = newNode(i);
+    }
+
+    for (int i = 0; i < NUM_EDGES; i++) {
+        edge_map[i].start->edges[edge_map[i].start->degree] = edge_map[i].index;
+        edge_map[i].start->degree++;
+        edge_map[i].end->edges[edge_map[i].end->degree] = edge_map[i].index;
+        edge_map[i].end->degree++;
+    }
+}
